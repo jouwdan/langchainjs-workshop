da6983bbc7efca092cb0ef21491c1230fbed285c
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-11-09 14:26:34 +0000

    fix: fixed app bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-  import { AppBar } from '@skeletonlabs/skeleton';
+  import { AppBar, AppShell } from '@skeletonlabs/skeleton';
   import '../app.postcss';
 </script>
 
-<AppBar>
-  <svelte:fragment slot="lead">
-    <a class="btn-icon btn-icon-sm variant-filled" href="/">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-        ><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7H4Z" /></svg
-      >
-    </a>
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar>
+      <svelte:fragment slot="lead">
+        <a class="btn-icon btn-icon-sm variant-filled" href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+            ><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7H4Z" /></svg
+          >
+        </a>
+      </svelte:fragment>
+      NodeBot
+    </AppBar>
   </svelte:fragment>
-  NodeBot
-</AppBar>
-<slot />
+  <slot />
+</AppShell>
